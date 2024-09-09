9acf41a33d6f53dcdec3de81633447f2e5b93d6d
JovanovaDanche <jovanovadanche@gmail.com>
2024-09-09 23:45:53 +0200

    Add initial project

---
+++ EBookStore.Domain/Domain/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBookStore.Domain.Domain
+{
+    public class Book : BaseEntity
+    {
+        [Required]
+        public String Title { get; set; }
+        [Required]
+        public string BookCover { get; set; }
+        public String Description { get; set; }
+        [Required]
+        public virtual ICollection<Author>? Authors { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
+        [Required]
+        public Publisher Publisher { get; set; }
+        [Required]
+        public String Genre { get; set; }
+        [Required]
+        public Int32 QuantityAvaiable { get; set; }
+        [Required]
+        public double Price { get; set; }
+    }
+}
---
+++ EBookStore.Domain/Domain/Order.cs
@@ -0,0 +1,16 @@
+﻿using EBookStore.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBookStore.Domain.Domain
+{
+    public class Order : BaseEntity
+    {
+        public string? OwnerId { get; set; }
+        public EBookStoreAppUser? Owner { get; set; }
+        public ICollection<BookInOrder>? BooksInOrders { get; set; }
+    }
+}
